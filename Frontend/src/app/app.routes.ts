6da3413c884edac4e3843893500aeac90fd944a5
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-06-18 00:15:55 +0300

    seprate thr login and egister component

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { ProfilePageComponent } from './_features/profile-page/profile-page.component';
 import { ProfileEditComponent } from "./_features/profile-edit/profile-edit.component";
+import { LoginComponent } from './_features/login/login.component';
+import { RegisterComponent } from "./_features/register/register.component";
+import { AuthenticationComponent } from "./_features/authentication/authentication.component";
+
 
 export const routes: Routes = [
   { path: 'profile', component: ProfilePageComponent },
   { path: 'profile/edit', component: ProfileEditComponent},
+  {
+    path: 'auth',
+    component: AuthenticationComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ]
+  },
+
 ];
