be4fd83f9b463f2eaec2a8702a3dc08fafc1fa72
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-06-20 00:16:15 +0200

    Enumarated Status added

--- Backend/UserService/src/main/java/org/spring/userservice/models/UserModel.java
+++ Backend/UserService/src/main/java/org/spring/userservice/models/UserModel.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Table(name = "UserModel", indexes = {
-		@Index(name = "idx_usermodel_username", columnList = "username")
+        @Index(name = "idx_usermodel_username", columnList = "username")
 })
 @Getter
 @Setter
@@ -17,45 +17,47 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserModel implements Serializable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Long id;
-	@Column(name = "username", nullable = false, unique = true)
-	private String username;
-	@Column(name = "password", nullable = false)
-	private String password;
-	@Email
-	@Column(name = "email", nullable = false, unique = true)
-	private String email;
-	@Enumerated(EnumType.ORDINAL)
-	private Status status;
-	private String firstName;
-	private String lastName;
-	private String address;
-	private Gender gender;
-	private String country;
-	private String city;
-	private LocalDate birthDate;
-	private String phoneNumber;
-	private String profilePicture;
-	private String coverPicture;
-	private String url;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Enumerated(EnumType.ORDINAL)
+    private Status status;
+    private String firstName;
+    private String lastName;
+    private String address;
+    @Enumerated(EnumType.ORDINAL)
+    private Gender gender;
+    private String country;
+    private String city;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private String profilePicture;
+    private String coverPicture;
+    // URL For any Portfolio or other Social Media platform
+    private String url;
 
-	public Integer getAge() {
-		return LocalDate.now().getYear() - birthDate.getYear();
-	}
+    public Integer getAge() {
+        return LocalDate.now().getYear() - birthDate.getYear();
+    }
 
-	@Override
-	public final boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		UserModel userModel = (UserModel) o;
-		return Objects.equals(id, userModel.id);
-	}
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id);
+    }
 
-	@Override
-	public final int hashCode() {
-		return Objects.hash(id);
-	}
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id);
+    }
 }
