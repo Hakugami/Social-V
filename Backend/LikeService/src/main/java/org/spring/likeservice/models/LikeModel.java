a546b29e854d2391531ebade9db7b14bbe6b437d
MennaMoataz23 <mennamansour297@gmail.com>
2024-06-22 01:54:51 +0300

    LikeService

---
+++ Backend/LikeService/src/main/java/org/spring/likeservice/models/LikeModel.java
@@ -0,0 +1,14 @@
+package org.spring.likeservice.models;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
+@Data
+public class LikeModel {
+    @MongoId
+    private String id;
+    private String postId;
+    private String userId;
+}
