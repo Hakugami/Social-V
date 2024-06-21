1e8f21fc5a8e45a2a1631c4b0859e41975d2263d
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2024-06-21 22:19:19 +0300

    dockerized some services

--- Backend/ApiGateway/src/main/java/org/spring/apigateway/clients/AuthServerClient.java
+++ Backend/ApiGateway/src/main/java/org/spring/apigateway/clients/AuthServerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "AUTHENTICATIONSERVER")
+@FeignClient(name = "authentication-server")
 public interface AuthServerClient {
 
 	@PostMapping("/auth/validate")
