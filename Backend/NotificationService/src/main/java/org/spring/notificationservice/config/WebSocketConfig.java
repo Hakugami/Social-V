8037794735645babd1a7be185a0046575e2f5c63
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2024-06-24 01:22:45 +0300

    fixes

--- Backend/NotificationService/src/main/java/org/spring/notificationservice/config/WebSocketConfig.java
+++ Backend/NotificationService/src/main/java/org/spring/notificationservice/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package org.spring.notificationservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,18 +10,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	@Value("${allowed.origins}")
+	private String allowedOrigins;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/user", "/topic");
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user");
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/user", "/topic");
+		config.setApplicationDestinationPrefixes("/app");
+		config.setUserDestinationPrefix("/user");
+	}
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:49920")
-                .withSockJS();
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+				.setAllowedOrigins(allowedOrigins)
+				.withSockJS();
+	}
 }
