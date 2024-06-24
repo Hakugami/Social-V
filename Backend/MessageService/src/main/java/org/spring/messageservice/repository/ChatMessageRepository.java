00ff6683002b19f2adab40479f01461be453c035
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-06-24 16:06:30 +0300

    build the infrastructure of the messaging service

---
+++ Backend/MessageService/src/main/java/org/spring/messageservice/repository/ChatMessageRepository.java
@@ -0,0 +1,12 @@
+package org.spring.messageservice.repository;
+
+
+import org.spring.messageservice.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    List<ChatMessage> findByChatId(String s);
+}
